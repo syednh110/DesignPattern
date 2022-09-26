201cce960f96f5e53ac1bc4c718ad0f03873c4dd
syednh110 <syednh110@gmail.com>
2022-09-26 19:20:41 +0530

    Added factorypattern and abstractpattern

---
+++ src/CreationalDesignPatterns/AbstractDesignPattern/AbstractDesignPattern.java
@@ -0,0 +1,4 @@
+package CreationalDesignPatterns.AbstractDesignPattern;
+
+public class AbstractDesignPattern {
+}
---
+++ src/CreationalDesignPatterns/FactoryDesign/FactoryDesignPattern.java
@@ -0,0 +1,4 @@
+package CreationalDesignPatterns.FactoryDesign;
+
+public class FactoryDesignPattern {
+}
